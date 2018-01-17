5f72663e4c34985579d8dd028e4514aa0f11a92f
Hilda <hilda.sandberg@gmail.com>
2018-01-17 21:42:30 +0100

    made galleryimage array dynamic

--- src/components/projects/gallery.js
+++ src/components/projects/gallery.js
@@ -6,26 +6,13 @@ import "./projects.css"
 class Gallery extends React.Component {
 
   render() {
-
-    // const image = {
-    //     original: this.props.gallImg1,
-    //     thumbnail: this.props.gallImg1
-    //   }
-
-    const images = [
-      {
-        original: this.props.gallImg1,
-        thumbnail: this.props.gallImg1
-      },
-      {
-        original: this.props.gallImg2,
-        thumbnail: this.props.gallImg2
-      },
-      {
-        original: this.props.gallImg3,
-        thumbnail: this.props.gallImg3
+    let images = []
+    images = this.props.gallImgAll.map((image) => {
+      return {
+        original: image,
+        thumbnail: image
       }
-    ]
+    })
 
     return (
       <ImageGallery
